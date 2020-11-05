06e467327d55d9c57a35391a79f6d0bc806b4a89
Joshua Shony <jushuashony@gmail.com>
2020-11-05 23:53:27 +0200

    'ok'

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,32 @@
-var gulp  = require('gulp');
-var sass  = require('gulp-sass');
-var watch = require('gulp-watch');
+var gulp        = require('gulp');
+var sass        = require('gulp-sass');
+// var watch       = require('gulp-watch');
+var browserSync = require('browser-sync').create();
+
+// sass.compiler = require('node-sass');
 
 // task para o sass
-gulp.task('sass', function(){
+function style(){
+	// 1. where is my sass file
 	return gulp.src('./sass/**/*.sass')
-	.pipe(sass().on('error', sass.logError))
-	.pipe(gulp.dest('./css'));
-});
+	// 2. passe that  file through sass compliler
+	.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+	// 3. where do i save the  compliled css
+	.pipe(gulp.dest('./css'))
+	// 4. stream changes to all browser
+	.pipe(browserSync.stream());
+}
 
-// task do watch
-gulp.task('watch', function () {
-  gulp.watch('./sass/**/*.sass', ['sass']);
-});
+function watch() {
+	browserSync.init({
+		server: {
+			baseDir: './'
+		}
+	});
+	gulp.watch('./sass/**/*.sass', style);
+	gulp.watch('./**/*.html').on('change', browserSync.reload);
+	gulp.watch('./js/**/*.js').on('change', browserSync.reload)
+}
 
-// task default
-gulp.task('default', function(){
-	
-});
+exports.style = style;
+exports.watch = watch;
